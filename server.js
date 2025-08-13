57d9c32bf6bfd845f23ac8a92423d581c24ee794
ecoregreso <72965951+ecoregreso@users.noreply.github.com>
2025-08-13 02:26:16 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-import express from "express";
-import cors from "cors";
+// server.js
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
 app.use(cors());
