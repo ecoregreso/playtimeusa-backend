d0ebca7412ee5301db5e508005f35361b7bab393
eco regreso <72965951+ecoregreso@users.noreply.github.com>
2025-09-02 08:45:41 -0700

    Delete models directory

--- models/Bet.js
+++
@@ -1,4 +0,0 @@
-const { DataTypes } = require('sequelize');
-const sequelize = require('../config/database');
-const Bet = sequelize.define('Bet',{ id:{type:DataTypes.BIGINT,primaryKey:true,autoIncrement:true}, playerId:{type:DataTypes.INTEGER}, gameId:{type:DataTypes.INTEGER}, stake:{type:DataTypes.DECIMAL(12,2)}, payout:{type:DataTypes.DECIMAL(12,2), defaultValue:0}, result:{type:DataTypes.JSON} });
-module.exports = Bet;
