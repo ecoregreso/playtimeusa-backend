2e791bd540425fa5b2a9dd076aaeea6568a743d1
eco regreso <72965951+ecoregreso@users.noreply.github.com>
2025-09-02 08:45:16 -0700

    Delete config directory

--- config/database.js
+++
@@ -1,3 +0,0 @@
-const { Sequelize } = require('sequelize');
-const sequelize = new Sequelize({ dialect: 'sqlite', storage: process.env.SQLITE_STORAGE || './database.sqlite', logging:false });
-module.exports = sequelize;
