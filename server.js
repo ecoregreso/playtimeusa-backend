2f445412ca48cc00491f6c92b459f68753ab9a61
ecoregreso <ecoregreso@gmail.com>
2025-09-02 11:23:55 -0700

    main

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 const express = require('express');
-const cors = require('cors');
 const QRCode = require('qrcode');
 const path = require('path');
 const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
 .then(() => console.log('MongoDB connected'))
 .catch(err => console.error('MongoDB connection error:', err));
+
 
 app.use(cors());
 
