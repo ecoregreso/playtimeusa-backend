3465bcaebf2ffffe47fd8fc7b54f2da416b20d2d
eco regreso <72965951+ecoregreso@users.noreply.github.com>
2025-10-06 12:42:21 -0700

    Update Voucher.js

--- Voucher.js
+++ Voucher.js
@@ -1,17 +1,1 @@
-const mongoose = require('mongoose');
-
-const voucherSchema = new mongoose.Schema(
-  {
-    userCode: { type: String, required: true, unique: true, index: true },
-    password: { type: String, required: true },
-    amount: { type: Number, required: true, min: 1 },
-    bonus: { type: Number, required: true, min: 0 },
-    balance: { type: Number, required: true, min: 0 },
-    isUsed: { type: Boolean, default: false }
-  },
-  {
-    timestamps: true
-  }
-);
-
-module.exports = mongoose.model('Voucher', voucherSchema);
+module.exports = require('./models/Voucher');
