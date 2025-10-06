ac59c792c393c083bc7e07075c925f6ebb4a26bd
eco regreso <72965951+ecoregreso@users.noreply.github.com>
2025-10-06 12:27:40 -0700

    Update Bet.js

--- models/Bet.js
+++ models/Bet.js
@@ -1,4 +1,48 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
-const Bet = sequelize.define('Bet',{ id:{type:DataTypes.BIGINT,primaryKey:true,autoIncrement:true}, playerId:{type:DataTypes.INTEGER}, gameId:{type:DataTypes.INTEGER}, stake:{type:DataTypes.DECIMAL(12,2)}, payout:{type:DataTypes.DECIMAL(12,2), defaultValue:0}, result:{type:DataTypes.JSON} });
+
+const Bet = sequelize.define(
+  'Bet',
+  {
+    id: {
+      type: DataTypes.BIGINT,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    playerId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    gameId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    stake: {
+      type: DataTypes.DECIMAL(12, 2),
+      allowNull: false,
+      defaultValue: 0
+    },
+    payout: {
+      type: DataTypes.DECIMAL(12, 2),
+      allowNull: false,
+      defaultValue: 0
+    },
+    result: {
+      type: DataTypes.JSON,
+      allowNull: true
+    }
+  },
+  {
+    tableName: 'Bets',
+    indexes: [
+      {
+        fields: ['playerId']
+      },
+      {
+        fields: ['gameId']
+      }
+    ]
+  }
+);
+
 module.exports = Bet;
